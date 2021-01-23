0336a18a84d889272ade0b2c619ab3a362d392f8
Jonathan Watson <jonathanw12@outlook.com>
2021-01-23 17:38:40 +0000

    Prettifying miles.js

--- src/miles.js
+++ src/miles.js
@@ -1,61 +1,61 @@
-let browser_longitude = 0;
-let browser_latitude = 0;
-
 const locationStringToLongitudeLatitude = async (locationString) => {
   /*
-  Use a google geoencoding api to convert the location string into longitude and latitude
-  */
+   Use a google geoencoding api to convert the location string into longitude and latitude
+   */
   var geocoder = new google.maps.Geocoder();
 
-  const {results} = await geocoder.geocode({ 'address': locationString });
+  const { results } = await geocoder.geocode({ 'address': locationString });
   const latitude = results[0].geometry.location.lat();
   const longitude = results[0].geometry.location.lng();
+
   return {
     latitude,
     longitude,
   };
 }
 
-const getBrowserLongitudeLatitude = () => {
-  /*
-   Use Geolocation API to get the browser longitude and latitude
-   Cally a helper function `returnBrowserLongitudeLatitude`
-  */
-  if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(returnLongitudeLatitude); // success callback
-  }
-  else{
-    console.log(`Geolocation is not supported by this browser.`);
-  }
-  return {
-    browser_longitude,
-    browser_latitude,
-  }
+const getPosition = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
-const returnLongitudeLatitude = (position) => {
+const getBrowserLongitudeLatitude = async () => {
   /*
-  Return the longitude and latitude for the position
-  */
-  browser_longitude = position.coords.longitude;
-  browser_latitude = position.coords.latitude;
+   Use Geolocation API to get the browser longitude and latitude
+   */
+  if (navigator.geolocation){
+    const { coords } = await getPosition();
+    const { longitude, latitude } = coords;
+
+    return {
+      longitude,
+      latitude,
+    }
+  }
+
+  console.log(`Geolocation is not supported by this browser.`);
+  return {
+    longitude: 0,
+    latitude: 0,
+  }
 }
 
 const getMilesBetweenLongitudeLatitudes = ({longitude1, latitude1}, {longitude2, latitude2}) => {
   /*
-  input: longitude, latitude of two positions in degrees
-  output: miles between the two locations
-  implementation detail: uses Haversine formula to determine the great-circle distance between two points on a sphere
-  */
- var R = 3958.8; // Radius of the Earth in miles
- var rlat1 = degreeToRadius(latitude1); // Convert degrees to radians
- var rlat2 = degreeToRadius(latitude2); // Convert degrees to radians
- var difflat = rlat2-rlat1; // Radian difference (latitudes)
- var difflon = degreeToRadius(longitude2-longitude1); // Radian difference (longitudes)
- var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
- return d;
+   input: longitude, latitude of two positions in degrees
+   output: miles between the two locations
+   implementation detail: uses Haversine formula to determine the great-circle distance between two points on a sphere
+   */
+
+  const R = 3958.8; // Radius of the Earth in miles
+  const rlat1 = degreeToRadius(latitude1); // Convert degrees to radians
+  const rlat2 = degreeToRadius(latitude2); // Convert degrees to radians
+  const difflat = rlat2 - rlat1; // Radian difference (latitudes)
+  const difflon = degreeToRadius(longitude2-longitude1); // Radian difference (longitudes)
+  return 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
 }
 
 const degreeToRadius = (deg) => {
-  return deg*Math.PI/180;
+  return deg * Math.PI / 180;
 }

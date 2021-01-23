cb57de6b4f6b93a4434bf266d4c85b4556851009
KyraZzz <yz709@cam.ac.uk>
2021-01-23 21:33:45 +0000

    update static file storage

--- backend/ecosystem/ecosystem/settings.py
+++ backend/ecosystem/ecosystem/settings.py
@@ -126,4 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CORS_ORIGIN_ALLOW_ALL = True

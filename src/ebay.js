5020140f667f86f0796712075c95dfac0649d1b0
Jonathan Watson <jonathanw12@outlook.com>
2021-01-23 12:39:59 +0000

    Implement getLocationStringFromUrl

--- src/ebay.js
+++ src/ebay.js
@@ -1,13 +1,28 @@
+import axios from 'axios';
+
 const getUrlsFromPage = (document) => {
   return [
-    "https://???"
+    'https://???'
   ];
 }
 
-const getLocationStringFromUrl = (url) => {
-  return "Framingham, Massachusetts, United States";
+const getLocationStringFromUrl = async (url) => {
+  const html = await axios.get(url);
+  const dom = new DOMParser().parseFromString(html, 'text/html');
+
+  const xpath = "//span[text()='Item location:']"
+  const matchingElement = dom.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE, null
+  ).singleNodeValue;
+  const textNode = matchingElement.nextSibling;
+  const text = textNode.data.trim();
+
+  return text;
 }
 
 const getHtmlFromLocationString = (locationString) => {
-  return "<div>5 leaves</div>";
+  return '<div>5 leaves</div>';
 }

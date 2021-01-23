59dac533da816069ee634e2ff51b899ffbee5a5e
KyraZzz <yz709@cam.ac.uk>
2021-01-23 22:05:08 +0000

    install django-heruku

--- backend/ecosystem/ecosystem/settings.py
+++ backend/ecosystem/ecosystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())

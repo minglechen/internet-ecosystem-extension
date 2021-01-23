6abfd8c078f40b1247399e92b11a58977b199109
Jonathan Watson <jonathanw12@outlook.com>
2021-01-23 13:11:10 +0000

    Implement getHtmlFromLocationString

--- src/ebay.js
+++ src/ebay.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+import { locationStringToLongitudeLatitude, getBrowserLongitudeLatitude, getMilesBetweenLongitudeLatitudes } from 'miles.js';
+import { milesToCarbon } from './carbon.js';
+import { getRating } from './rating';
+
 const getUrlsFromPage = (document) => {
   return [
     'https://???'
@@ -24,5 +28,14 @@ const getLocationStringFromUrl = async (url) => {
 }
 
 const getHtmlFromLocationString = (locationString) => {
-  return '<div>5 leaves</div>';
+  const longitudeLatitude1 = locationStringToLongitudeLatitude(locationString);
+  const longitudeLatitude2 = getBrowserLongitudeLatitude();
+  const miles = getMilesBetweenLongitudeLatitudes(longitudeLatitude1, longitudeLatitude2);
+  const carbon = milesToCarbon();
+  const { rating, visualisation } = getRating(miles, carbon);
+
+  const leafUrl = "https://static.thenounproject.com/png/1882848-200.png";
+  const leafHtml = `<img src="${leafUrl}" alt="Leaf" width="32px" height="32px" />`;
+
+  return `<div>${leafHtml.repeat(rating)}</div>`;
 }
